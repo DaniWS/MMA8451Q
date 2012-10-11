edb957e10dc6ff630c4c87aa524b4ecd0e674ebf
samux <samux@localhost>
2012-10-11 14:26:22 +0000

    modify doxygen comments/add license

--- MMA8451Q.cpp
+++ MMA8451Q.cpp
@@ -1,24 +1,42 @@
+/* Copyright (c) 2010-2011 mbed.org, MIT License
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+* and associated documentation files (the "Software"), to deal in the Software without
+* restriction, including without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all copies or
+* substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+* BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+* DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #include "MMA8451Q.h"
 
-#define REG_WHO_AM_I       0x0D
-#define REG_CTRL_REG_1     0x2A
+#define REG_WHO_AM_I      0x0D
+#define REG_CTRL_REG_1    0x2A
 #define REG_OUT_X_MSB     0x01
 #define REG_OUT_Y_MSB     0x03
 #define REG_OUT_Z_MSB     0x05
 
-#define UINT14_MAX 16383
+#define UINT14_MAX        16383
 
 MMA8451Q::MMA8451Q(PinName sda, PinName scl, int addr) : m_i2c(sda, scl), m_addr(addr) {
     // activate the peripheral
     uint8_t data[2] = {REG_CTRL_REG_1, 0x01};
-    write_regs(data, 2);
+    writeRegs(data, 2);
 }
 
 MMA8451Q::~MMA8451Q() { }
 
 uint8_t MMA8451Q::getWhoAmI() {
     uint8_t who_am_i = 0;
-    read_regs(REG_WHO_AM_I, &who_am_i, 1);
+    readRegs(REG_WHO_AM_I, &who_am_i, 1);
     return who_am_i;
 }
 
@@ -43,7 +61,7 @@ void MMA8451Q::getAccAllAxis(int16_t * res) {
 int16_t MMA8451Q::getAccAxis(uint8_t addr) {
     int16_t acc;
     uint8_t res[2];
-    read_regs(addr, res, 2);
+    readRegs(addr, res, 2);
 
     acc = (res[0] << 6) | (res[1] >> 2);
     if (acc > UINT14_MAX/2)
@@ -52,12 +70,12 @@ int16_t MMA8451Q::getAccAxis(uint8_t addr) {
     return acc;
 }
 
-void MMA8451Q::read_regs(int addr, uint8_t * data, int len) {
+void MMA8451Q::readRegs(int addr, uint8_t * data, int len) {
     char t[1] = {addr};
     m_i2c.write(m_addr, t, 1, true);
     m_i2c.read(m_addr, (char *)data, len);
 }
 
-void MMA8451Q::write_regs(uint8_t * data, int len) {
+void MMA8451Q::writeRegs(uint8_t * data, int len) {
     m_i2c.write(m_addr, (char *)data, len);
 }
--- MMA8451Q.h
+++ MMA8451Q.h
@@ -1,68 +1,106 @@
+/* Copyright (c) 2010-2011 mbed.org, MIT License
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+* and associated documentation files (the "Software"), to deal in the Software without
+* restriction, including without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all copies or
+* substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+* BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+* DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #ifndef MMA8451Q_H
 #define MMA8451Q_H
 
 #include "mbed.h"
 
-//!Library for the MMA8451Q motion sensor.
+/**
+* MMA8451Q accelerometer example
+* #include "mbed.h"
+* #include "MMA8451Q.h"
+*
+* #define MMA8451_I2C_ADDRESS (0x1d<<1)
+*
+* int main(void) {
+*    DigitalOut led(LED_GREEN);
+*    MMA8451Q acc(P_E25, P_E24, MMA8451_I2C_ADDRESS);
+*    printf("WHO AM I: 0x%2X\r\n", acc.getWhoAmI());
+*    
+*    while (true) {
+*        printf("-----------\r\n");
+*        printf("acc_x: %d\r\n", acc.getAccX());
+*        printf("acc_y: %d\r\n", acc.getAccY());
+*        printf("acc_z: %d\r\n", acc.getAccZ());
+*        
+*        wait(1);
+*        led = !led;
+*    }
+* }
+*/
 class MMA8451Q
 {
 public:
-  //!Creates an instance of the class.
-  /*
-  * Connect module at I2C address addr using I2C port pins sda and scl.
-  * MMA8451Q
-  * \param sda SDA pin
-  * \param sdl SCL pin
-  * \param addr addr of the I2C peripheral
+  /**
+  * MMA8451Q constructor
+  *
+  * @param sda SDA pin
+  * @param sdl SCL pin
+  * @param addr addr of the I2C peripheral
   */
   MMA8451Q(PinName sda, PinName scl, int addr);
 
-  /*
-  * Destroys instance.
+  /**
+  * MMA8451Q destructor
   */
   ~MMA8451Q();
 
-  /*
+  /**
    * Get the value of the WHO_AM_I register
    *
-   * \returns WHO_AM_I value
+   * @returns WHO_AM_I value
    */
   uint8_t getWhoAmI();
 
-  /*
+  /**
    * Get X axis acceleration
    *
-   * \returns X axis acceleration
+   * @returns X axis acceleration
    */
   int16_t getAccX();
 
-  /*
+  /**
    * Get Y axis acceleration
    *
-   * \returns Y axis acceleration
+   * @returns Y axis acceleration
    */
   int16_t getAccY();
 
-  /*
+  /**
    * Get Z axis acceleration
    *
-   * \returns Z axis acceleration
+   * @returns Z axis acceleration
    */
   int16_t getAccZ();
 
-  /*
+  /**
    * Get XYZ axis acceleration
    *
-   * \param res array where acceleration data will be stored
+   * @param res array where acceleration data will be stored
    */
-  void    getAccAllAxis(int16_t * res);
-
-  I2C m_i2c;
+  void getAccAllAxis(int16_t * res);
 
 private:
+  I2C m_i2c;
   int m_addr;
-  void read_regs(int addr, uint8_t * data, int len);
-  void write_regs(uint8_t * data, int len);
+  void readRegs(int addr, uint8_t * data, int len);
+  void writeRegs(uint8_t * data, int len);
   int16_t getAccAxis(uint8_t addr);
 
 };
